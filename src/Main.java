1a7805e0e959d7bd514ed1dc1a713d1f1a0702dd
Dylan3004 <dawid.palka@op.pl>
2023-12-16 19:50:17 +0100

    done

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainFrame();
+
+    }
+}
